b74bdbcc21e47b791b5949d2049c491999253728
arkady <larytet@yahoo.com>
2019-07-11 14:07:23 +0300

    Update

--- hamming.py
+++ hamming.py
@@ -23,13 +23,67 @@ import logging
 import sys
 import os
 import re
-try:
-    from ruamel.yaml import YAML
-    from docopt import docopt
-except:
-    print "Try pip install -r requirements.txt"
-    exit(1)    
-import glob
-import socket
-import string
-import collections 
+from docopt import docopt
+
+def open_file(filename, flags, print_error=True):
+    '''
+    Open a text file 
+    Returns handle to the open file and result code False/True
+    '''
+    try:
+        file_handle = open(filename, flags) 
+    except Exception:
+        if print_error:
+            logger.error('Failed to open file {0} {1}'.format(filename, sys.exc_info()))
+        return (False, None)
+    else:
+      return (True, file_handle) 
+
+def hamming(data_set, max_distance):
+  '''
+  Create dictionary of hashes with hamming distance less than max_distance
+  '''
+  pass
+
+
+
+def read_data_set(f):
+  '''
+  Read the file line by line, convert hexadecimals to intergs, return the list
+  '''
+  data = []
+  for l in f:
+    data.append(int(l, 16))
+
+  return data
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__, version='0.1')
+    logging.basicConfig()    
+    logger = logging.getLogger('hamming')
+    logger.setLevel(logging.INFO) 
+
+
+    data_file = arguments['--file']
+    distance_arg = arguments['--distance']
+
+    while True:
+
+        if data_file is None:
+          logger.info("Nothing to do")
+          break
+
+        max_distance = 32
+        if not distance_arg is None:
+          max_distance = int(distance_arg)
+
+
+        result, f = open_file(data_file, "r")
+        if not result:
+          break        
+        
+        data_set = read_data_set(f)
+        logger.debug("Read {0} hashes".format(len(data_set)))
+        hamming_distances = hamming(f, max_distance)
+        break
+
